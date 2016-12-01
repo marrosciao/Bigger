db15d1448b532266787affcaa82427631d92887b
rikard-helgegren <rikard.helgegren@gmail.com>
2016-12-01 17:58:20 +0100

    Update Character.java

--- Character.java
+++ Character.java
@@ -2,6 +2,9 @@ public class Character{
     private String name;
     private int hitPoints;
     private String [] bag; //Primarely only allow 10 items.
+    private new Item [] equipment;
+    private int attack;
+    private int defence;
     private String description;
     
     public Character(){
