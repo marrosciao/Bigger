20b08850801dd65313aa1f30a71cf80cd22870dc
rikard-helgegren <rikard.helgegren@gmail.com>
2016-12-01 12:12:04 +0100

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,11 @@
+public class Player{
+    private String name;
+    private Int HitPoints;
+    private String [] bag; //Primarely only allow 10 items.
+    
+    public Player(){
+    
+    }
+    
+    
+}
