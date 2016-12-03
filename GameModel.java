a44f8369fc3055e3775d32d53d9acd3e6dcde4ba
rikard-helgegren <rikard.helgegren@gmail.com>
2016-12-03 17:39:02 +0100

    Add files via upload

---
+++ GameModel.java
@@ -0,0 +1,20 @@
+
+public class GameModel {
+
+    public GameModel() {
+        
+    	setUpGame();
+    }
+
+    /*
+     * Creates the world by reading a txt-file describing the world with key words.
+     * This in order to able that the player adds code to the came that can be added directly 
+     */
+    private void setUpGame(){
+    	
+    	
+    	
+    	
+    	
+    }
+}
